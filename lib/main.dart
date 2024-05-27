33744203863be4399456d48c2428a0c1edcca600
HP <harikrishnan290604@outlook.com>
2024-05-27 19:11:31 +0530

    I commit the code

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gemini/flutter_gemini.dart';
 import 'package:nexara/consts.dart';
 import 'package:nexara/welcome.dart';
-
 void main()  {
   Gemini.init(
     apiKey: GEMINI_API_KEY,
   );
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
